0902c5060f9f187b05cffdd76f9656394897f985
Cobrettie <cobrettiegarner3@gmail.com>
2020-04-17 19:43:00 -0400

    style PlayerCard Component

--- client/src/Components/PlayerCard/PlayerCard.js
+++ client/src/Components/PlayerCard/PlayerCard.js
@@ -1,20 +1,21 @@
 import React from 'react'
+import { CardContainerDiv, PlayerCardDiv } from './PlayerCardStyles'
 
 export default function PlayerCard(props) {
   console.log(props.players)
   return(
     <div>
       <h2>Player Card Component</h2>
-      <div>
+      <CardContainerDiv>
         {props.players.map(player => {
           return (
-            <div key={player.id} >
+            <PlayerCardDiv key={player.id} >
               <p>Name: {player.name}</p>
               <p>Country: {player.country}</p>
-            </div> 
+            </PlayerCardDiv> 
           )          
         })}
-      </div>
+      </CardContainerDiv>
     </div>
   )
 }
---
+++ client/src/Components/PlayerCard/PlayerCardStyles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+export const CardContainerDiv = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+`;
+
+export const PlayerCardDiv = styled.div`
+  border: 1px solid #373737;
+  width: 15%;
+  padding: 10px;
+  margin: 20px;
+  border-radius: 5px;
+`;
